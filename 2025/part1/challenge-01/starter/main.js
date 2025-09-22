769428ec47d161a2229d3e4dd142610e37043b46
alex <907785012@qq.com>
2025-09-22 16:16:02 +0800

    add starter of challenge-01

---
+++ 2025/part1/challenge-01/starter/main.js
@@ -0,0 +1,9 @@
+/*
+  1. Sync input value with Angular signals and show it on the label(ngModel two-way binding)
+  2. Implement reset(add event listener and handler function)
+  3. Apply transform style to the cube(computed expression)
+*/
+
+// {
+//   transform: `perspective(0px)rotateX(0deg)rotateY(0deg)rotateZ(0deg)`,
+// }
