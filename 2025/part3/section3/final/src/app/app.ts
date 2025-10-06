5e3859198cfe69cfc7e238260b481a530f7f01fc
alex <907785012@qq.com>
2025-10-06 13:52:16 +0800

    add starter of http-client section

---
+++ 2025/part3/section3/final/src/app/app.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+})
+export class App implements OnInit {
+  currentAdvice = signal('');
+  isLoading = signal(false);
+
+  async handleGetAdvice() {
+    this.isLoading.set(true);
+
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json();
+
+    this.currentAdvice.set(data.slip.advice);
+
+    this.isLoading.set(false);
+  }
+
+  ngOnInit() {
+    this.handleGetAdvice();
+  }
+}
