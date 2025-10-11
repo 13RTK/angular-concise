c0e0ceb3df08c098e8363f106cc73b1f7bb0cfe6
alex <907785012@qq.com>
2025-10-11 18:46:19 +0800

    complete http-client section

--- 2025/part3/section3/final/src/app/app.ts
+++ 2025/part3/section3/final/src/app/app.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+
+type AdviceResponse = {
+  slip: {
+    id: number;
+    advice: string;
+  };
+};
 
 @Component({
   selector: 'app-root',
@@ -8,15 +16,25 @@ export class App implements OnInit {
   currentAdvice = signal('');
   isLoading = signal(false);
 
-  async handleGetAdvice() {
+  http = inject(HttpClient);
+
+  handleGetAdvice() {
     this.isLoading.set(true);
 
-    const response = await fetch('https://api.adviceslip.com/advice');
-    const data = await response.json();
-
-    this.currentAdvice.set(data.slip.advice);
-
-    this.isLoading.set(false);
+    this.http
+      .get<AdviceResponse>('https://api.adviceslip.com/advice', {
+        timeout: 3000,
+      })
+      .subscribe({
+        next: (advice) => {
+          this.currentAdvice.set(advice.slip.advice);
+          this.isLoading.set(false);
+        },
+        error: (err) => {
+          alert(err.message);
+          this.isLoading.set(false);
+        },
+      });
   }
 
   ngOnInit() {
