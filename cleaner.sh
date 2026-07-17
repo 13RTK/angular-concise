0e27755457344747ce659f1092c190f45030c937
alex <907785012@qq.com>
2026-07-17 20:57:59 +0800

    update cleaner

--- cleaner.sh
+++ cleaner.sh
@@ -2,4 +2,4 @@
 find . -type d -name "node_modules" -exec rm -rf {} \;
 
 # Find all pnpm-lock.json files and delete them
-find . -type f -name "pnpm-lock.yaml" -exec rm -rf {} \;
+# find . -type f -name "pnpm-lock.yaml" -exec rm -rf {} \;
