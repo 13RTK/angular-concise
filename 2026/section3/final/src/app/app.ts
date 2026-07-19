ff53fbcd5a885dfca186291e187a4dccdb0fe0b7
alex <907785012@qq.com>
2026-07-19 19:27:21 +0800

    complete section1-3

--- 2026/section3/final/src/app/app.ts
+++ 2026/section3/final/src/app/app.ts
@@ -4,15 +4,56 @@ import { Component, signal } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>Hello From Angular</h1>
-    <input (input)="handleInput($event)" type="text" />
-    <p>{{ inputText().split('').reverse().join('') }}</p>
+    <form (submit)="handleSubmit($event)">
+      <input
+        type="text"
+        [value]="username()"
+        name="username"
+        (input)="username.set($event.target.value.trim())"
+      />
+      <br />
+      <br />
+      <input
+        type="password"
+        [value]="password()"
+        name="password"
+        (input)="password.set($event.target.value.trim())"
+      />
+      <br />
+      <br />
+
+      <button [disabled]="username() === '' || password() === ''" type="submit">Submit</button>
+    </form>
   `,
   styleUrl: './app.css',
 })
 export class App {
-  inputText = signal('');
+  username = signal('');
+  password = signal('');
 
-  handleInput(event: Event) {
-    this.inputText.set((event.target as HTMLInputElement).value);
+  handleSubmit(event: Event) {
+    event.preventDefault();
+
+    // Signal version
+    alert(`Username: ${this.username()}\nPassword: ${this.password()}`);
+    this.username.set('');
+    this.password.set('');
+    return;
+
+    // Native formData version
+    // const formElement = event.target as HTMLFormElement;
+    // const formData = new FormData(formElement);
+
+    // const username = formData.get('username')?.toString().trim();
+    // const password = formData.get('password')?.toString().trim();
+
+    // if (!username || !password) {
+    //   alert('Please enter both username and password.');
+    //   return;
+    // }
+
+    // alert(`Username: ${username}\nPassword: ${password}`);
+    // formElement.reset();
+    // return;
   }
 }
