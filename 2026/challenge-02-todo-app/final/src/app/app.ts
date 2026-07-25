7bba09983aa8304e68f18c36b04c610176ace2df
alex <907785012@qq.com>
2026-07-25 11:40:13 +0800

    complete challenge-02

--- 2026/challenge-02-todo-app/final/src/app/app.ts
+++ 2026/challenge-02-todo-app/final/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 /*
  * Goal:
@@ -18,4 +18,60 @@ import { Component } from '@angular/core';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  todoInput = signal('');
+
+  todos = signal<
+    {
+      id: number;
+      content: string;
+      isCompleted: boolean;
+    }[]
+  >([]);
+
+  competedTodos = computed(() =>
+    this.todos().sort((o1, o2) => {
+      if (o1.isCompleted !== o2.isCompleted) {
+        return o1.isCompleted ? 1 : -1;
+      }
+
+      return o1.id - o2.id;
+    }),
+  );
+
+  handleAddTodo() {
+    this.todos.set([
+      ...this.todos(),
+      { id: Date.now(), content: this.todoInput(), isCompleted: false },
+    ]);
+
+    this.todoInput.set('');
+  }
+
+  handleToggleTodo(id: number) {
+    this.todos.set(
+      this.todos().map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isCompleted: !todo.isCompleted,
+          };
+        }
+
+        return todo;
+      }),
+    );
+  }
+
+  handleRemoveTodo(id: number) {
+    this.todos.set(
+      this.todos().filter((todo) => {
+        if (todo.id === id) {
+          return false;
+        }
+
+        return true;
+      }),
+    );
+  }
+}
