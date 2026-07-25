e2209dd96611227226c918c8cd439ed75fb8ab88
alex <907785012@qq.com>
2026-07-25 11:11:56 +0800

    add starter of challenge02

---
+++ 2026/challenge-02-todo-app/final/src/app/app.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+/*
+ * Goal:
+ * 1. Create signals and bind them to the template
+ * 2. Render the list with @for + track, and an @empty placeholder
+ * 3. Implement add / remove / toggle
+ */
+
+/*
+ * @name Todo App Challenge
+ * @version 2.0
+ * @author HDAlex_John
+ */
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {}
