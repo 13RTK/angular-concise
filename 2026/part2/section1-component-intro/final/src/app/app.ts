672eafc5328dea4eaea998a7f6e6f83acfeaff4b
alex <907785012@qq.com>
2026-07-27 12:09:20 +0800

    complete section2-1

--- 2026/part2/section1-component-intro/final/src/app/app.ts
+++ 2026/part2/section1-component-intro/final/src/app/app.ts
@@ -1,25 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { form, FormField, max, min } from '@angular/forms/signals';
-
-/*
- * Goal:
- * 1. Separate the template into html file
- * 2. Create several signals, bind them with the template
- * 3. Bind the style of circle box with the signals
- * Hint: {
-      width: `${this.size()}px`,
-      height: `${this.size()}px`,
-      backgroundColor: `${this.color()}`,
-      transform: `rotate(${this.rotate()}deg)`,
-    }
- * 4. Implement the reset button
- */
-
-/*
- * @name Dynamic Circle Challenge
- * @version 2.0
- * @author HDAlex_John
- */
+import { CircleColor } from './circle-color/circle-color';
 
 interface CircleData {
   circleColor: string;
@@ -39,7 +20,7 @@ const circleDataInitialValue: CircleData = {
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [FormField],
+  imports: [FormField, CircleColor],
 })
 export class App {
   circleModel = signal<CircleData>(circleDataInitialValue);
