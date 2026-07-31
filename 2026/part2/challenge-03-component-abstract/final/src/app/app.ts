c94198f13d7b9ad7985e6b0c26fc9b8368bd064e
alex <907785012@qq.com>
2026-07-31 12:06:43 +0800

    add starter of challenge03

---
+++ 2026/part2/challenge-03-component-abstract/final/src/app/app.ts
@@ -0,0 +1,40 @@
+import { Component, signal } from '@angular/core';
+import { form, FormField, max, min } from '@angular/forms/signals';
+import { CircleColor } from './circle-color/circle-color';
+import { CircleText } from './circle-text/circle-text';
+import { CircleSize } from './circle-size/circle-size';
+import { CircleRotate } from './circle-rotate/circle-rotate';
+
+interface CircleData {
+  circleColor: string;
+  circleText: string;
+  circleSize: number;
+  circleRotate: number;
+}
+
+const circleDataInitialValue: CircleData = {
+  circleColor: '#45d619',
+  circleText: 'Hi!',
+  circleSize: 150,
+  circleRotate: 0,
+};
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+  imports: [CircleColor, CircleText, CircleSize, CircleRotate],
+})
+export class App {
+  circleModel = signal<CircleData>(circleDataInitialValue);
+
+  circleForm = form(this.circleModel, (schemaPath) => {
+    min(schemaPath.circleSize, 0);
+    min(schemaPath.circleRotate, 0);
+    max(schemaPath.circleRotate, 360);
+  });
+
+  handleReset() {
+    this.circleForm().value.set(circleDataInitialValue);
+  }
+}
