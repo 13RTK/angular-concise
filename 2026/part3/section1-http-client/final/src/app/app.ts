735e06738f260a81e93ed0618fbc5b7d2dbfca8d
alex <907785012@qq.com>
2026-08-01 14:08:45 +0800

    complete section3-1

--- 2026/part3/section1-http-client/final/src/app/app.ts
+++ 2026/part3/section1-http-client/final/src/app/app.ts
@@ -1,7 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+
+type AdviceSlip = {
+  slip: {
+    id: number;
+    advice: string;
+  };
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
 })
-export class App {}
+export class App {
+  advice = signal('');
+  isLoading = signal(false);
+
+  httpClient = inject(HttpClient);
+
+  getAdvice() {
+    this.isLoading.set(true);
+
+    this.httpClient.get<AdviceSlip>('https://api.adviceslip.com/advice').subscribe((adviceSlip) => {
+      this.advice.set(adviceSlip.slip.advice);
+      this.isLoading.set(false);
+    });
+  }
+}
