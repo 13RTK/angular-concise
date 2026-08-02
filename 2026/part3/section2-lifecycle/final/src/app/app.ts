03af3a42cef56538ec5359579e449b245078018c
alex <907785012@qq.com>
2026-08-02 15:55:32 +0800

    add starter of section3-2

---
+++ 2026/part3/section2-lifecycle/final/src/app/app.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+
+type AdviceSlip = {
+  slip: {
+    id: number;
+    advice: string;
+  };
+};
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+})
+export class App {
+  advice = signal('');
+  isLoading = signal(false);
+
+  httpClient = inject(HttpClient);
+
+  getAdvice() {
+    this.isLoading.set(true);
+
+    this.httpClient.get<AdviceSlip>('https://api.adviceslip.com/advice').subscribe((adviceSlip) => {
+      this.advice.set(adviceSlip.slip.advice);
+      this.isLoading.set(false);
+    });
+  }
+}
