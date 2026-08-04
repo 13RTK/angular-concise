6af0be78489a7a7e6e19366d77c0e323ad373add
alex <907785012@qq.com>
2026-08-04 21:32:56 +0800

    add starter of cinemata

---
+++ 2026/cinemata/starter/src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Navbar } from './navbar/navbar';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+  imports: [Navbar],
+})
+export class App {}
