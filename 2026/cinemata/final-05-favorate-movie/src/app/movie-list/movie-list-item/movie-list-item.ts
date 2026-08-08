3d7a0365a62e5621ef85a8bb7f1977f0333adcf9
alex <907785012@qq.com>
2026-08-08 14:12:19 +0800

    complete final05

--- 2026/cinemata/final-05-favorate-movie/src/app/movie-list/movie-list-item/movie-list-item.ts
+++ 2026/cinemata/final-05-favorate-movie/src/app/movie-list/movie-list-item/movie-list-item.ts
@@ -1,7 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Movie } from '@/types/Movie';
+import { FavoriteMovieStore } from '@/app/favorite-movie-store';
 
 @Component({
   selector: 'movie-list-item',
@@ -11,4 +12,17 @@ import { Movie } from '@/types/Movie';
 })
 export class MovieListItem {
   movie = input.required<Movie>();
+
+  favoriteMovieStore = inject(FavoriteMovieStore);
+
+  isFavoriteMovie = computed(() => this.favoriteMovieStore.isFavoriteMovie(this.movie().id));
+
+  toggleFavoriteMovie() {
+    if (this.isFavoriteMovie()) {
+      this.favoriteMovieStore.removeFavoriteMovie(this.movie().id);
+      return;
+    }
+
+    this.favoriteMovieStore.addFavoriteMovie(this.movie());
+  }
 }
